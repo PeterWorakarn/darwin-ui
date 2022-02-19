aeb8349b11bce2cffd556f03ffe919efb2ac4bae
Worakarn Oun <skboardgames@gmail.com>
2022-02-19 22:05:47 +0700

    feat: :lipstick: Implement Scroll Smooth and Selector Color

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,14 @@
-import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
+import Document, {
+  DocumentContext, Head, Html, Main, NextScript,
+} from 'next/document';
 
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang='en'>
-        <Head>
-        </Head>
-        <body>
+      <Html style={{ scrollBehavior: 'smooth' }} lang="en">
+        <Head />
+        {/* TODO: Scroll Smooth, Selector Color,  */}
+        <body className="selection:bg-[#EF4444] selection:text-white scroll-bar">
           <Main />
           <NextScript />
         </body>
