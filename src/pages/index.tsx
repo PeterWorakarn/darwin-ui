d8843e506ea2e9ab97bb788b989c5b024f247e51
Worakarn Oun <skboardgames@gmail.com>
2022-03-01 16:52:03 +0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
@@ -11,7 +9,7 @@ const Home: NextPage = () => {
         </h1>
       </main>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
