2221df38671a7bf06f5e41cf3115660746b01129
Worakarn Oun <skboardgames@gmail.com>
2022-03-01 16:50:59 +0700

    [Feature] Implement Checkbox

---
+++ doc/checkbox/Checkbox.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { BsCheckLg } from 'react-icons/bs';
+
+const Checkbox: React.FC = () => {
+  const [checkboxValue, setCheckboxValue] = useState(true);
+
+  return (
+    <label htmlFor="checkbox-name" className="flex gap-2 items-center justify-start">
+      <div
+        className={`w-5 h-5 rounded flex items-center justify-center 
+        ${checkboxValue ? 'border border-primary-navy bg-primary-navy ' : 'border-2 border-gray-200'} `}
+      >
+        <BsCheckLg
+          className={`text-white bg-transparent w-3.5 h-3.5 transition-all duration-150 ease-out 
+          ${checkboxValue ? 'opacity-100' : 'opacity-0'}`}
+        />
+      </div>
+      <input
+        id="checkbox-name"
+        name="checkbox-name"
+        className="sr-only"
+        type="checkbox"
+        onClick={() => setCheckboxValue((prev) => !prev)}
+        checked={checkboxValue}
+        readOnly
+      />
+      <span
+        className={`transition-all duration-150 select-none text-xl text-[#6e6e6e] 
+        ${checkboxValue ? 'font-medium' : 'font-normal'}`}
+      >
+        Option
+      </span>
+    </label>
+  );
+};
+
+export default Checkbox;
---
+++ src/pages/components/checkbox.tsx
@@ -0,0 +1,67 @@
+import type { NextPage } from 'next';
+import fs from 'fs';
+import path from 'path';
+import { TBlockType, TSourceCode } from '../../constant-enum-type/doc';
+import ShowCaseBlock from '../../feature/showcase/components/ShowCaseBlock';
+import HeaderBlock from '../../feature/showcase/components/HeaderBlock';
+import Checkbox from '../../../doc/checkbox/Checkbox';
+
+const title = 'checkbox';
+const short = 'checkbox';
+const component_name = 'checkbox';
+const blockType:TBlockType = TBlockType.SQUARE;
+const component = (
+  <Checkbox />
+);
+const markDown = `
+  # Hello world
+`;
+
+export const getStaticProps = async () => {
+  const files = fs.readdirSync(
+    path.join('doc', component_name),
+  );
+  const doc = files.map((filename) => {
+    // Read File From Server Side and Return to Array
+    // How to know their are many file in it ?
+    const file_content = fs.readFileSync(
+      path.join('doc', component_name, filename),
+      'utf-8',
+    );
+    return {
+      file_content,
+      file_name: filename,
+    };
+  });
+
+  return {
+    props: {
+      doc,
+    },
+  };
+};
+
+interface DocProps {
+  doc: TSourceCode[];
+}
+
+const DatePickerPage: NextPage<DocProps> = ({
+  doc,
+}) => {
+  return (
+    <>
+      <HeaderBlock
+        title={title}
+        short={short}
+        markDown={markDown}
+      />
+      <ShowCaseBlock
+        blockType={blockType}
+        component={component}
+        doc={doc}
+      />
+    </>
+  );
+};
+
+export default DatePickerPage;
