9e9bcd8e5a2b6d84900dfc8253b8aa34ec32d168
Worakarn Oun <skboardgames@gmail.com>
2022-03-21 09:29:21 +0700

    Init Google Analytics

---
+++ src/config/index.ts
@@ -0,0 +1,2 @@
+export const gaTrackingCode = 'G-NJNQ2BJQED';
+export default {};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,29 @@
 import '../styles/tailwind.css';
 import type { AppProps } from 'next/app';
+import * as ga from 'react-ga';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import HeadSEO from '../feature/seo/HeadSEO';
 import Layout from '../feature/common/components/Layout';
+import { gaTrackingCode } from '../config';
+
+ga.initialize(gaTrackingCode);
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+  useEffect(() => {
+    // Run Logic on GA
+    ga.pageview(window.location.pathname);
+    router.events.on('routeChangeStart', () => {
+      setIsLoading(true);
+    });
+
+    router.events.on('routeChangeComplete', () => {
+      setIsLoading(false);
+      ga.pageview(window.location.pathname);
+    });
+  }, []);
   return (
     <>
       <HeadSEO />
