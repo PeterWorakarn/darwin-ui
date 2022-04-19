7de13bd085e7a98aadc8c33eb63c34bef2ed4028
Worakarn Oun <skboardgames@gmail.com>
2022-04-19 17:16:48 +0700

    Update aDrawerProvider.tsx

--- doc/drawer/aDrawerProvider.tsx
+++ doc/drawer/aDrawerProvider.tsx
@@ -42,8 +42,15 @@ const DrawerProvider: React.FC = (props) => {
       value={{ ...drawerStore }}
     >
       {props.children}
-      <div onClick={() => setDrawerStatus({ content: null, status: false })} className={`transition-all ease-in duration-150 flex fixed w-screen h-screen inset-0 z-[60] ${drawerStatus.status ? 'opacity-30 bg-primary-navy' : 'opacity-0 bg-primary-navy invisible'}`} />
-      <article className={`${drawerStatus.status ? 'translate-y-[200px] xl:translate-y-[200px] opacity-100' : 'translate-y-[500px] xl:translate-y-[1000px] opacity-10'} xl:-translate-x-60 transition-all ease-in duration-150 fixed z-[70] flex-shrink-0 w-full max-w-xs h-1/2 xl:h-3/4 rounded-xl shadow pt-7 px-5 pb-4 mx-auto bg-white }`}>
+      <div
+        onClick={() => setDrawerStatus({ content: null, status: false })}
+        className={`transition-all ease-in duration-150 flex fixed w-screen h-screen inset-0 z-[60] 
+        ${drawerStatus.status ? 'opacity-30 bg-primary-navy' : 'opacity-0 bg-primary-navy invisible'}`}
+      />
+      <article
+        className={`${drawerStatus.status ? 'translate-y-[200px] xl:translate-y-[200px] opacity-100' : 'translate-y-[500px] xl:translate-y-[1000px] opacity-10'} 
+      xl:-translate-x-60 transition-all ease-in duration-150 fixed z-[70] flex-shrink-0 w-full max-w-xs h-1/2 xl:h-3/4 rounded-xl shadow pt-7 px-5 pb-4 mx-auto bg-white`}
+      >
         {drawerStatus.content}
       </article>
     </DrawerContext.Provider>
