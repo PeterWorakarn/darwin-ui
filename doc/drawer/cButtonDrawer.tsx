e0dd1d5139b435a5926c619b3448780f834a4bfa
Worakarn Oun <skboardgames@gmail.com>
2022-05-15 23:02:03 +0700

    change icons

--- doc/drawer/cButtonDrawer.tsx
+++ doc/drawer/cButtonDrawer.tsx
@@ -38,7 +38,9 @@ const ButtonDrawer: React.FC = () => {
         <div className="flex justify-center items-center
       gap-1 font-medium uppercase"
         >
-          <FiActivity size={18} />
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z" />
+          </svg>
           Drawer
         </div>
       </button>
